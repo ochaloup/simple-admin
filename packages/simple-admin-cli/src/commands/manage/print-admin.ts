2199bf20b79ba25eb59ce29720278ca1f9e455b0
Ondra Chaloupka <chalda@marinade.finance>
2023-04-05 18:09:48 +0200

    print-admin CLI: permit to use keypair as address

--- packages/simple-admin-cli/src/commands/manage/print-admin.ts
+++ packages/simple-admin-cli/src/commands/manage/print-admin.ts
@@ -1,19 +1,21 @@
 import {
-  parsePubkey,
   parsePubkeyOrKeypair,
 } from '@marinade.finance/solana-cli-utils'
 import { Keypair, PublicKey } from '@solana/web3.js'
 import { Command } from 'commander'
 import { useContext } from '../../context'
-import { withPrintAdmin } from 'simple-admin-sdk'
+import { simpleAccount, withPrintAdmin } from 'simple-admin-sdk'
 import { executeTx } from '../utils'
-import { TransactionEnvelope } from '@saberhq/solana-contrib'
+import {
+  TransactionEnvelope,
+  TransactionReceipt,
+} from '@saberhq/solana-contrib'
 
 export function installPrintAdmin(program: Command) {
   program
     .command('print-admin')
     .description('Print a message when you are an admin.')
-    .argument('address', 'Address of simple account', parsePubkey)
+    .argument('address', 'Address of simple account ', parsePubkeyOrKeypair)
     .requiredOption(
       '--admin <keypair_or_pubkey>',
       'Admin of the account that provides the permission to print',
@@ -22,7 +24,7 @@ export function installPrintAdmin(program: Command) {
     .requiredOption('--message <string>', 'Message to print')
     .action(
       async (
-        address: Promise<PublicKey>,
+        address: Promise<PublicKey | Keypair>,
         {
           admin,
           message,
@@ -45,13 +47,22 @@ async function managePrintAdmin({
   admin,
   message,
 }: {
-  address: PublicKey
+  address: PublicKey | Keypair
   admin: PublicKey | Keypair
   message: string
 }) {
   const { sdk, provider, logger } = useContext()
 
+  address = address instanceof PublicKey ? address : address.publicKey
   const adminAddress = admin instanceof PublicKey ? admin : admin.publicKey
+
+  const simpleAccountData = await simpleAccount({ sdk, address })
+  if (!simpleAccountData.admin.equals(adminAddress)) {
+    throw new Error(
+      `Admin ${adminAddress.toBase58()} is not an admin of simple account ${address.toBase58()} ` +
+        `(admin is ${simpleAccountData.admin.toBase58()})`
+    )
+  }
 
   const tx = new TransactionEnvelope(provider, [], [])
   if (admin instanceof Keypair) {
@@ -64,11 +75,14 @@ async function managePrintAdmin({
     message,
   })
 
-  await executeTx(
+  const sig = await executeTx(
     tx,
-    `'Failed to print ${message} at chain at account ${address.toBase58()} with admin ${adminAddress.toBase58()}`
+    `'Failed to print message '${message}' at account ${address.toBase58()} with admin ${adminAddress.toBase58()}`
   )
   logger.info(
-    `Message ${message} succesfully printed for account ${address.toBase58()}`
+    `Message '${message}' succesfully printed for account ${address.toBase58()}` +
+      (sig instanceof TransactionReceipt
+        ? ` with signature ${sig.signature}`
+        : '')
   )
 }
