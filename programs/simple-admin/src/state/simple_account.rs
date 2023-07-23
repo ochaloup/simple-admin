dbb4efa66fc1246b5457198186118d8eb3ff6756
Ondra Chaloupka <chalda@marinade.finance>
2023-07-23 02:20:13 +0200

    adding todo on simple account data

--- programs/simple-admin/src/state/simple_account.rs
+++ programs/simple-admin/src/state/simple_account.rs
@@ -4,6 +4,7 @@ use anchor_lang::prelude::*;
 #[derive(Debug)]
 pub struct SimpleAccount {
     pub admin: Pubkey,
+    // TODO: print call count can be removed, not needed (plus u64 is nonsense when other is u32)
     pub print_call_count: u64,
     pub created_account_next_index: u32,
 }
